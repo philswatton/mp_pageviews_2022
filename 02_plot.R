8ec7d6d02ec8ba22a2a5fb68bd9e8c435144ba3d
philswatton <philswatton@gmail.com>
2022-04-23 19:18:14 +0100

    init commit

---
+++ 02_plot.R
@@ -0,0 +1,31 @@
+# MP Wikipedia Data Pageviews ----
+# Phil Swatton
+# University of Essex
+# File 02: Visualise pageview data
+
+
+## Packages
+library(tidyverse)
+library(treemap)
+
+
+## Data
+mps_total <- readRDS("data/mps_total.rds")
+color <- data.frame(party2 = c("Conservative Party", "Labour Party", "Scottish National", "Liberal Democrats",
+                               "Independent", "Democratic Unionist Party", "Green Party of England and Wales",
+                               "Plaid Cymru", "Social Democratic and Labour Party", "Sinn Féin", "Alliance"),
+                    color = c("#00aeef", "#e4003b", "#FFF481", "#fbb347",
+                              "#000000", "#19283E", "#6ab023",
+                              "#215F55", "#006E51", "#247F3F", "#F4C72E"))
+mps_total <- left_join(mps_total, color)
+
+
+# Treemap ----
+treemap(mps_total,
+        c("party2","name"),
+        vSize="views",
+        type="color",
+        vColor="color",
+        border.col="#FFFFFF",
+        bg.labels="#FFFFFF",
+        overlap.labels=0)
